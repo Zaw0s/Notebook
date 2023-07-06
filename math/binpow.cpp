685a66172eccde540ef3d1bfb92af6c5348c7852
Alberto Isaac Estrella Madrigal <aiem7aiem@gmail.com>
2023-07-06 11:15:09 -0600

    first file in notebook

---
+++ math/binpow.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+using namespace std;
+using namespace __gnu_pbds;
+using ll=long long;
+using ld=long double;
+using vi=vector<int>;
+template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+
+ll binpow( ll a, ll b){
+    ll ans = 1;
+    ll mul = a;
+    while( b > 0 ){
+        if( b & 1 ){
+            ans = ( ans * mul )%MOD;
+        }
+        mul = ( mul * mul )%MOD;
+    }
+    return ans;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
