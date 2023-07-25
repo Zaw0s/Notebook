d823ca747a7317cb971c84bef77ad7262a7bc42e
Alberto Isaac Estrella Madrigal <aiem7aiem@gmail.com>
2023-07-24 19:46:11 -0600

    new examples

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+using namespace std;
+using namespace __gnu_pbds;
+using ll=long long;
+using ld=long double;
+using vi=vector<int>;
+template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
